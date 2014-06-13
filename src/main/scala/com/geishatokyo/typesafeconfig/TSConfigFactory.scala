57f0b9bea17b160fce06ab1684cd4a72b08b91a1
Takeshita Yoshiteru <takezoux2@gmail.com>
2014-06-14 00:37:34 +0900

    Add factory methods

--- src/main/scala/com/geishatokyo/typesafeconfig/TSConfigFactory.scala
+++ src/main/scala/com/geishatokyo/typesafeconfig/TSConfigFactory.scala
@@ -1,27 +1,45 @@
 package com.geishatokyo.typesafeconfig
 
-import com.typesafe.config.ConfigFactory
+import com.typesafe.config.{Config, ConfigFactory}
 import com.geishatokyo.typesafeconfig.lax.LaxTSConfigRoot
+import java.io.File
 
 /**
  * Created by takezoux2 on 2014/06/13.
  */
 trait TSConfigFactory {
   def parseString(str : String) : TSConfig
+  def parseFile(path : String) : TSConfig
+  def parseFile(file : File) : TSConfig
+  def fromConfig(config : Config) : TSConfig
 }
 
-object TSConfigFactory extends Proxy {
+object TSConfigFactory extends TSConfigFactory{
 
   var defaultFactory = lax
 
-  def self = defaultFactory
-
-
+  override def parseString(str: String): TSConfig = defaultFactory.parseString(str)
+  override def fromConfig(config: Config): TSConfig = defaultFactory.fromConfig(config)
+  override def parseFile(path: String): TSConfig = defaultFactory.parseFile(path)
+  override def parseFile(file: File): TSConfig = defaultFactory.parseFile(file)
 
   object lax extends TSConfigFactory{
     override def parseString(str: String): TSConfig = {
       val conf = ConfigFactory.parseString(str)
       LaxTSConfigRoot(conf)
+    }
+
+    override def parseFile(path: String): TSConfig = {
+      val conf = ConfigFactory.parseFile(new File(path))
+      LaxTSConfigRoot(conf)
+    }
+    override def parseFile(file: File): TSConfig = {
+      val conf = ConfigFactory.parseFile(file)
+      LaxTSConfigRoot(conf)
+    }
+
+    override def fromConfig(config: Config): TSConfig = {
+      LaxTSConfigRoot(config)
     }
   }
 
